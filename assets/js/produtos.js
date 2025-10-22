8690a7c17416dbf37048cf21da3eb084a18fa764
HOME <hausmann.home@hotmail.com>
2025-10-21 23:28:10 -0300

    add: página de produtos e components

---
+++ assets/js/produtos.js
@@ -0,0 +1,23 @@
+// navbar toggling
+const navbarShowBtn = document.querySelector('.navbar-show-btn');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+const navbarHideBtn = document.querySelector('.navbar-hide-btn');
+
+if(navbarShowBtn && navbarCollapseDiv && navbarHideBtn){
+    navbarShowBtn.addEventListener('click', function(){
+        navbarCollapseDiv.classList.add('navbar-show');
+    });
+    navbarHideBtn.addEventListener('click', function(){
+        navbarCollapseDiv.classList.remove('navbar-show');
+    });
+}
+
+// stopping all animation and transition on resize
+let resizeTimer;
+window.addEventListener('resize', () =>{
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
